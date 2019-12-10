e0e74ed649a3dcf6c95e948e001099bd7eb3e1f3
Nicholas Jardine <nicholasjardine0@gmail.com>
2019-12-10 16:15:34 +0200

    added initial validations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,8 @@
 class Message < ApplicationRecord
   belongs_to :messageble, polymorphic: true
   belongs_to :user
+
+  validates :content, presence: true
+
+
 end
--- app/models/session.rb
+++ app/models/session.rb
@@ -2,4 +2,12 @@ class Session < ApplicationRecord
   belongs_to :lobby
   belongs_to :user
   has_many :messages, as: :messageable
+  has_many :user_invites
+  has_many :users, through: :user_invites
+
+validates :enter_price, presence: true
+validates :winning_price, presence: true
+validates :platform, presence: true
+
+
 end
