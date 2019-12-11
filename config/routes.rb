ce0b7fe01f701019208f77e547cb0ab5d462ed4f
Alexandru Sima <simaalexandru98@gmail.com>
2019-12-11 11:44:30 +0100

    home is the lobbies index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  resources :lobbies, only: [:index, :show] do
+  root to: 'lobbies#index'
+  resources :lobbies, only: [:show] do
     resources :sessions, only: [:index, :show, :new, :create]
   end
 
