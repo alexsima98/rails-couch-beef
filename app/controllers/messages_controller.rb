a41f7d45f2296ff68087aae77d70a10d72cea056
Nicholas Jardine <nicholasjardine0@gmail.com>
2019-12-11 23:14:48 +0200

    a little more

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,30 @@
 class MessagesController < ApplicationController
+  def create
+    @message = Message.new(message_params)
+    @message.user = current_user
+    @message.session = @session
+    if @message.save
+      redirect_to lobby_session(@lobby,@session)
+    else
+      render :new
+    end
+  end
+
+   private
+
+  def message_params
+    params.require(:session).permit(:price, :win_price, :platform)
+  end
 end
+
+
+  def create
+    @session = Session.new(session_params)
+    @session.user = current_user
+    @session.lobby = @lobby
+    if @session.save
+      redirect_to lobby_session(@lobby,@session)
+    else
+      render :new
+    end
+  end
