13fc0173a4b54fd0eb5bd213b08f8eb461edeffb
Nicholas Jardine <nicholasjardine0@gmail.com>
2019-12-13 15:44:26 +0200

    tbc

--- db/migrate/20191212131007_add_columns_to_sessions.rb
+++ db/migrate/20191212131007_add_columns_to_sessions.rb
@@ -1,9 +1,9 @@
 class AddColumnsToSessions < ActiveRecord::Migration[5.2]
   def change
-    add_column :sessions, :star_level, :string
-    add_column :sessions, :team_type, :string
-    add_column :sessions, :half_length, :string
-    add_column :sessions, :legacy_depending, :string
+    add_column :sessions, :star_level, :string, default: 'Any'
+    add_column :sessions, :team_type, :string, default: 'Any'
+    add_column :sessions, :half_length, :string, default: '6 min'
+    add_column :sessions, :legacy_depending, :string, default: 'Not allowed'
     add_column :sessions, :custom_rules, :text
   end
 end
