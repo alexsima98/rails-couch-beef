8c55d524de0c50e03343feb6966aab6372059baf
Roy Souvereyns <roy_souvereyns@hotmail.com>
2019-12-17 16:03:20 +0100

    status pending in card

--- app/controllers/user_invites_controller.rb
+++ app/controllers/user_invites_controller.rb
@@ -21,6 +21,7 @@ class UserInvitesController < ApplicationController
     @user_invite.status = 'accepted'
     @user_invite.save
     @session.status = 'in progress'
+    @session.save
     redirect_to dashboard_path(current_user)
   end
 
@@ -29,7 +30,6 @@ class UserInvitesController < ApplicationController
     @user_invite = UserInvite.find(params[:user_invite_id])
     @user_invite.status = "accepted"
     @user_invite.save
-    @session.status = "in progress"
     redirect_to dashboard_path(current_user)
   end
 
