080dbeccccab435ed450a60e25f57cbe9aae1f2e
Nicholas Jardine <nicholasjardine0@gmail.com>
2019-12-18 16:44:21 +0200

    not in the lobby and changes in dashboard when accepted

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,5 +3,6 @@ class DashboardsController < ApplicationController
     @mysessions = Session.where(user: current_user)
     @sessions = UserInvite.where(user: current_user).map{|ui| ui.session}
     @alls = @mysessions + @sessions
+    @inprogresssessions = @alls.select {|session| session.status == "in progress"}
   end
 end
