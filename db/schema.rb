ae2a44e8256b3d9ea258ca94135e02049185f4b3
Alexandru Sima <simaalexandru98@gmail.com>
2019-12-18 15:46:53 +0100

    delete session problems

--- db/schema.rb
+++ db/schema.rb
@@ -48,8 +48,8 @@ ActiveRecord::Schema.define(version: 2019_12_17_172606) do
     t.string "match_type", default: "Single game"
     t.string "status", default: "pending"
     t.string "winner"
-    t.integer "scoreOne", default: 0
-    t.integer "scoreTwo", default: 0
+    t.integer "score1", default: 0
+    t.integer "score2", default: 0
     t.index ["lobby_id"], name: "index_sessions_on_lobby_id"
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
