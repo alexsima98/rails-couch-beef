f641504920a679095eba131a4cd15ad21e7ae280
Roy Souvereyns <roy_souvereyns@hotmail.com>
2019-12-19 12:11:46 +0100

    dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,9 +1,10 @@
 class DashboardsController < ApplicationController
   def show
     @mysessions = Session.where(user: current_user)
-    @sessions = UserInvite.where(user: current_user).map{|ui| ui.session}
-    @alls = @mysessions + @sessions
-    @inprogresssessions = @alls.select {|session| session.status == "in progress"}
-    @finished_sessions = @alls.select {|session| session.status == "finished"}
+    @sessions = UserInvite.where(user: current_user).where(status: "Pending").map { |ui| ui.session }
+    @accepted_sessions = UserInvite.where(user: current_user).where(status: "accepted").map { |ui| ui.session }
+    @alls = @mysessions + @accepted_sessions
+    @inprogresssessions = @alls.select { |session| session.status == "in progress" || session.status == "finished" }
+    @finished_sessions = @alls.select { |session| session.status == "agreed" }
   end
 end
